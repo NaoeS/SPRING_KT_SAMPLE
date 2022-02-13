da22a451c72f7ab8b3e495725e0eed07248f0ace
NaoeS <hitsolb111081@gmail.com>
2022-02-13 18:57:12 +0900

    更新・削除APIを追加

--- src/main/kotlin/com/book/manager/infrastructure/database/repository/BookRepositoryImpl.kt
+++ src/main/kotlin/com/book/manager/infrastructure/database/repository/BookRepositoryImpl.kt
@@ -29,19 +29,19 @@ class BookRepositoryImpl(
     override fun findWithRental(id: Long): BookWithRental? {
         return bookWithRentalMapper.selectByPrimaryKey(id)?.let { toModel(it) }
     }
-//
-//    override fun register(book: Book) {
-//        bookMapper.insert(toRecord(book))
-//    }
-//
-//    override fun update(id: Long, title: String?, author: String?, releaseDate: LocalDate?) {
-//        bookMapper.updateByPrimaryKeySelective(BookRecord(id, title, author, releaseDate))
-//    }
-//
-//    override fun delete(id: Long) {
-//        bookMapper.deleteByPrimaryKey(id)
-//    }
-//
+
+    override fun register(book: Book) {
+        bookMapper.insert(toRecord(book))
+    }
+
+    override fun update(id: Long, title: String?, author: String?, releaseDate: LocalDate?) {
+        bookMapper.updateByPrimaryKeySelective(BookRecord(id, title, author, releaseDate))
+    }
+
+    override fun delete(id: Long) {
+        bookMapper.deleteByPrimaryKey(id)
+    }
+
     private fun toModel(record: BookWithRentalRecord): BookWithRental {
         val book = Book(
             record.id!!,
@@ -59,8 +59,8 @@ class BookRepositoryImpl(
         }
         return BookWithRental(book, rental)
     }
-//
-//    private fun toRecord(model: Book): BookRecord {
-//        return BookRecord(model.id, model.title, model.author, model.releaseDate)
-//    }
+
+    private fun toRecord(model: Book): BookRecord {
+        return BookRecord(model.id, model.title, model.author, model.releaseDate)
+    }
 }
